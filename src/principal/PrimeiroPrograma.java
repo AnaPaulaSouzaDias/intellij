b516d48725f2fd872a3f7772fe4ea304c31d8c8c
Ana Paula Souza <anapaula_sdias@hotmail.com>
2021-11-03 21:17:09 -0300

    desafio

---
+++ src/principal/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        int a =3;
+        int b =50;
+
+        System.out.println("Hellow Word " +(a+b));
+
+    }
+}
